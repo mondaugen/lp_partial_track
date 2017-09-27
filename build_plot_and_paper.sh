4ae6fc1c64e34e223d69fb62ebb196e69cabe81a
mondaugen <nicholas.esterer@gmail.com>
2017-09-27 14:11:24 -0400

    specify python before python script

--- build_plot_and_paper.sh
+++ build_plot_and_paper.sh
@@ -16,7 +16,7 @@ source setup_pythonpath.sh
 # build plot by first synthesizing test file
 test/make_chirps_simple.sh
 # then track partials and plot results
-test/bplp_dp_mq_simple.py
+python test/bplp_dp_mq_simple.py
 
 # now you can build the paper
 cd paper
