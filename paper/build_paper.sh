7f1ec392043b177ee0bea32d9047ec7a29906bc2
Nicholas Esterer <nicholas.esterer@gmail.com>
2019-01-15 12:33:11 -0500

    Added conditional building of paper for Archivx
    This basically just removes the header but still uses the WASPAA style.

--- paper/build_paper.sh
+++ paper/build_paper.sh
@@ -4,9 +4,18 @@
 
 # Convert all eps files to pdf
 
-pdflatex paper.tex
+outpdf_name=Esterer_Nicholas_WASPAA_2017.pdf
+[[ $BUILD_FOR_ARCHIVX ]] && outpdf_name=Esterer_Nicholas_archivx.pdf
+
+# set BUILD_FOR_ARCHIVX=1 for formatting for Archivx
+paper_build ()
+{
+    echo '\def\buildforarchivx{'"$BUILD_FOR_ARCHIVX"'} \input{paper.tex}'
+    pdflatex '\def\buildforarchivx{'"$BUILD_FOR_ARCHIVX"'} \input{paper.tex}'
+}
+paper_build
 BIBINPUTS=. bibtex paper.aux
-pdflatex paper.tex
-pdflatex paper.tex
-mv paper.pdf Esterer_Nicholas_WASPAA_2017.pdf
+paper_build
+paper_build
+mv paper.pdf "$outpdf_name"
 #dvipdf thesis.dvi
